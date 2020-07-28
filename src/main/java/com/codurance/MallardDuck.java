9b193070861bc321afa43ef92a20a4d683f09ad2
Tom <thomas.spencer@codurance.com>
2020-07-28 11:42:04 +0100

    Add Flyable

--- src/main/java/com/codurance/MallardDuck.java
+++ src/main/java/com/codurance/MallardDuck.java
@@ -1,8 +1,13 @@
 package com.codurance;
 
-public class MallardDuck extends Duck {
+public class MallardDuck extends Duck implements Flyable{
   @Override
   public void display(){
     System.out.println("I look like a mallard!");
+  }
+
+  @Override
+  public void fly() {
+    System.out.println("I am flying! I am a mallard!");
   }
 }
--- src/main/java/com/codurance/RedheadDuck.java
+++ src/main/java/com/codurance/RedheadDuck.java
@@ -1,8 +1,13 @@
 package com.codurance;
 
-public class RedheadDuck extends Duck {
+public class RedheadDuck extends Duck implements Flyable{
   @Override
   public void display(){
     System.out.println("I look like a Redhead!");
+  }
+
+  @Override
+  public void fly() {
+    System.out.println("I am flying! I am a redhead!");
   }
 }
