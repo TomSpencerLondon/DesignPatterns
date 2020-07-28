68289825ce6bce8fe1620e5d4e8767e36ae7fff7
Tom <thomas.spencer@codurance.com>
2020-07-28 12:29:15 +0100

    Add new Quack and new Fly instances

--- src/main/java/com/codurance/DecoyDuck.java
+++ src/main/java/com/codurance/DecoyDuck.java
@@ -2,6 +2,11 @@ package com.codurance;
 
 public class DecoyDuck extends Duck {
 
+  public DecoyDuck() {
+    flyBehavior = new FlyNoWay();
+    quackBehavior = () -> {};
+  }
+
   @Override
   public void display() {
     System.out.println("I look a bit wooden! I am a decoy!");
